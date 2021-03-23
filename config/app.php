98ae4936a606ddfcb933c59b957eae0b08685be2
GKentaurus <gkent@outlook.com>
2021-03-22 21:33:55 -0500

    Configuraciones para deploy en Heroku

--- config/app.php
+++ config/app.php
@@ -165,8 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+      
         /*
          * Application Service Providers...
          */
