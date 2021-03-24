7341673b13f107cf827014335334d51c2e570b6c
danniela <danny94rh@gmail.com>
2021-03-23 22:35:29 -0500

    modificando index principal 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,15 +11,14 @@
     <div class="col-12">
       <h1>Hola k ase</h1>
       <div class="col-4">
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 18rem;background-color: #ff7800;box-shadow: 4px 4px 8px #606060;">
           <div class="card-body">
             <h5 class="card-title">Trabajos</h5>
             <p class="card-text">
-              Pendientes <h5></h5>
-              <br>
-              Finalizados <h5></h5>
+              Pendientes <h5>({{ $openJobs->count()}})</h5>
+              Finalizados <h5> </h5>
             </p>
-            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Ver</a>
           </div>
         </div>
       </div>
