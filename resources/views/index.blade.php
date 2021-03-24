13ca56639ef5bb27f92b1fe6d51c061a7675014d
danniela <danny94rh@gmail.com>
2021-03-23 22:02:39 -0500

    modificando index principal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,22 @@
 <div class="container mt-3">
   <div class="row">
     <div class="col-12">
-      <h1>Hola</h1>
+      <h1>Hola k ase</h1>
+      <div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Trabajos</h5>
+            <p class="card-text">
+              Pendientes <h5></h5>
+              <br>
+              Finalizados <h5></h5>
+            </p>
+            <a href="#" class="card-link">Card link</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-4"></div>
+      <div class="col-4"></div>
     </div>
   </div>
 {{--  Footer --}}
