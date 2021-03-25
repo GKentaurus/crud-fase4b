4271f66ef518c01f8395ce2288710146713be240
danniela <danny94rh@gmail.com>
2021-03-24 19:14:36 -0500

    modificando index principal 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,43 +8,41 @@
 @include('templates.navbar')
 <div class="container mt-3">
   
-<div class="row">
-    <div class="col-12">
-      <h1 class="title">Dashboard</h1>
-      <div class="col-4">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">Trabajos</h5>
-            <p class="card-text">
-              <span class="cant">{{ $totalOpenJobs}}</span>
-              <br>
-              Pendientes
-            </p>
-          </div>
+  <h1 class="title">Dashboard</h1>
+  <div class="row">
+    <div class="col-lg-4">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Trabajos</h5>
+          <p class="card-text">
+            <span class="cant">{{ $totalOpenJobs}}</span>
+            <br>
+            Pendientes
+          </p>
         </div>
       </div>
-      <div class="col-4">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">Trabajos</h5>
-            <p class="card-text">
-              <span class="cant">{{ $totalClosedJobs}}</span>
-              <br>
-              Finalizados
-            </p>
-          </div>
+    </div>
+    <div class="col-lg-4">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Trabajos</h5>
+          <p class="card-text">
+            <span class="cant">{{ $totalClosedJobs}}</span>
+            <br>
+            Finalizados
+          </p>
         </div>
       </div>
-      <div class="col-4">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">Trabajos</h5>
-            <p class="card-text">
-              <span class="cant">{{ $totalVehicules}}</span>
-              <br>
-              Vehiculos
-            </p>
-          </div>
+    </div>
+    <div class="col-lg-4">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text">
+            <span class="cant">{{ $totalVehicules}}</span>
+            <br>
+            Vehiculos
+          </p>
         </div>
       </div>
     </div>
