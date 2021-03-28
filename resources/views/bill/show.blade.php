47a54ff7180f3987199c84f611bf485c228de61e
danniela <danny94rh@gmail.com>
2021-03-28 12:31:29 -0500

    factura

--- resources/views/bill/show.blade.php
+++ resources/views/bill/show.blade.php
@@ -9,98 +9,65 @@
 <div class="container">
   <div class="row">
     <div class="col l12 cont">
-      <h3>Información de la factura</h3>
-      <h3>Información del cliente</h3>
-      <div class="input-field col l6 s12">
-        <input type="text" id="customer_information" name="customer_information"
-               aria-describedby="customer_information_help"
-               value="{{$customer['firstname']}} {{$customer['lastname']}}"
-               disabled>
-        <label for="customer_information">Propietario del vehículo</label>
+      <h3>Información de la factura # {{$bill['id']}}</h3>
+      <div class="col l4 s6">
+        <h3>Información del cliente</h3>
+          <p class="white-text">
+          {{$customer['firstname']}} {{$customer['lastname']}}
+          <br>
+          {{$customer['document_type']}} {{$customer['document_number']}}
+          <br>
+          {{$customer['address']}}
+          <br>
+          {{$customer['phone_number']}}
+          </p>
       </div>
-
-      <div class="input-field col l6 s12">
-        <input type="text" id="customer_information" name="customer_information"
-               aria-describedby="customer_information_help"
-               value="{{$customer['document_type']}} {{$customer['document_number']}}"
-               disabled>
-        <label for="customer_information">Identificación del propietario</label>
-      </div>
-
-      <div class="input-field col l6 s12">
-        <input type="text" id="customer_information" name="customer_information"
-               aria-describedby="customer_information_help"
-               value="{{$customer['address']}}"
-               disabled>
-        <label for="customer_information">Dirección de facturación</label>
-      </div>
-
-      <div class="input-field col l6 s12">
-        <input type="text" id="customer_information" name="customer_information"
-               aria-describedby="customer_information_help"
-               value="{{$customer['phone_number']}}"
-               disabled>
-        <label for="customer_information">Número telefónico</label>
-      </div>
-    </div>
-    <div class="col l12 cont">
+    <div class="col l4 s6 offset-l4">
       <h3>Información del vehículo</h3>
-      <div class="input-field col l6 s12">
-        <input type="text" id="vehicule_information" name="vehicule_information"
-               aria-describedby="vehicule_information" value="{{$vehicule['license_plate']}}" disabled>
-        <label for="vehicule_information">Matrícula del vehículo</label>
-      </div>
-
-      <div class="input-field col l6 s12">
-        <input type="text" id="vehicule_information" name="vehicule_information"
-               aria-describedby="vehicule_information" value="{{$vehicule['brand']}}" disabled>
-        <label for="vehicule_information">Marca</label>
-      </div>
-
-      <div class="input-field col l6 s12">
-        <input type="text" id="vehicule_information" name="vehicule_information"
-               aria-describedby="vehicule_information" value="{{$vehicule['model']}}" disabled>
-        <label for="vehicule_information">Modelo</label>
-      </div>
-
-      <div class="input-field col l12">
-        <input type="text" id="vehicule_information" name="vehicule_information"
-               aria-describedby="vehicule_information" value="{{$vehicule['color']}}" disabled>
-        <label for="vehicule_information">Color</label>
-      </div>
+        <p class="white-text">
+          {{$vehicule['brand']}} {{$vehicule['model']}}
+          <br>
+          {{$vehicule['color']}}
+          <br>
+          {{$vehicule['license_plate']}}
+        </p>
     </div>
-    <div class="col s12">
-      <h1>Desglose</h1>
-      @foreach($job->jobDetails as $detail)
-        <div>OT # {{$detail['job_id']}}</div>
-        <div>Detalle de # {{$detail['id']}}</div>
-        <div>Mecánico: {{$detail->employee['firstname'] . ' ' . $detail->employee['lastname']}}</div>
-        <div>Parte intervenida {{$detail['intervened_part']}}</div>
-        <div>Costo de la parte: {{$detail['part_cost']}}</div>
-        <div>Mano de obra: {{$detail['workforce_cost']}}</div>
-        <div>Subtotal: {{$detail['part_cost'] + $detail['workforce_cost']}}</div>
-        <hr>
-      @endforeach
-    </div>
-    <div class="col l12 cont">
-      <h3>Totales de la Factura</h3>
-      <div class="input-field col l6 s12">
-        <input type="text" id="subtotal" name="subtotal" aria-describedby="subtotal_help"
-               value="{{$bill['total_cost']}}" disabled>
-        <label for="subtotal">Subtotal</label>
-      </div>
-
-      <div class="input-field col l6 s12">
-        <input type="text" id="tax" name="tax" aria-describedby="tax_help"
-               value="{{$bill['total_tax']}}" disabled>
-        <label for="tax">Impuestos (IVA 19%)</label>
-      </div>
-
-      <div class="input-field col s12">
-        <input type="text" id="total" name="total" aria-describedby="total_help"
-               value="{{$bill['total_cost'] + $bill['total_tax']}}" disabled>
-        <label for="total">Total</label>
-      </div>
+  </div>
+  <div class="col s12">
+    <table>
+      <thead>
+      <tr>
+        <th>OT #</th>
+        <th>Parte intervenida</th>
+        <th>Mecánico</th>
+        <th>Costo de la parte</th>
+        <th>Mano de obra</th>
+        <th>Subtotal</th>
+      </tr>
+      </thead>
+      <tbody>
+        @foreach($job->jobDetails as $detail)
+        <tr>
+          <td>{{$detail['job_id']}}</td>
+          <td>{{$detail['intervened_part']}}</td>
+          <td>{{$detail->employee['firstname'] . ' ' . $detail->employee['lastname']}}</td>
+          <td>{{$detail['part_cost']}}</td>
+          <td>{{$detail['workforce_cost']}}</td>
+          <td>{{$detail['part_cost'] + $detail['workforce_cost']}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  <div class="col l4 s6 offset-l4">
+    <h3>Totales de la Factura</h3>
+    <p class="white-text">
+      Subtotal: ${{$bill['total_cost']}}
+      <br>
+      Impuestos (IVA 19%): ${{$bill['total_tax']}}
+      <br>
+      Total: ${{$bill['total_cost'] + $bill['total_tax']}}
+    </p>
       <div class="col l12" style="display: contents;">
         <a href="{{ route('customer.show', $customer['id']) }}"
            class="btn btn-small waves-effect waves-light blue darken-1">Ver cliente</a>
